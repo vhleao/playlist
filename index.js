9ff315555ebdaacbedd9944bb955b115b4c1710d
vhleao <vhleao@gmail.com>
2022-03-29 19:23:49 -0300

    connect db

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express')
+const connectToDb = require("./database/db")
+
+
+
+const app = express()
+const port = process.env.PORT || 3000; 
+
+
+connectToDb();
+
+app.get("/", (req, res) =>{
+    res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`Servidor Rodando em http://localhost:${port}`))
