4e89c6e70c30b23e07a0efa758e5d1307256e138
vhleao <vhleao@gmail.com>
2022-03-29 20:14:55 -0300

    getALL

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 const Music = require("./model/Music");
 const res = require("express/lib/response");
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,23 +12,22 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-
 connectToDb();
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const playlist = await Music.find();
+  res.render("index", { playlist });
 });
 
-app.get("/admin", (req, res) =>{
-    res.render("admin")
+app.get("/admin", (req, res) => {
+  res.render("admin");
 });
 
 app.post("/create", async (req, res) => {
-    const music = req.body;
-    await Music.create(music);
-    res.redirect("/");
-  });
-
+  const music = req.body;
+  await Music.create(music);
+  res.redirect("/");
+});
 
 app.listen(port, () =>
   console.log(`Servidor Rodando em http://localhost:${port}`)
